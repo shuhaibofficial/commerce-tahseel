b993abe8efad8cbe7dd9d9e47342920356d1b412
Shuhaib Areekkan <homeshuhaib@gmail.com>
2020-10-04 14:04:18 +0300

    Onsite PG Implemntation with payment status pending

---
+++ src/Plugin/Commerce/PaymentGateway/TahseelDirectInterface.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Drupal\commerce_tahseel\Plugin\Commerce\PaymentGateway;
+
+use Drupal\commerce_payment\Plugin\Commerce\PaymentGateway\OnsitePaymentGatewayInterface;
+use Drupal\Commerce\PaymentGateway\SupportsNotificationsInterface
+
+interface TahseelDirectInterface extends OnsitePaymentGatewayInterface, SupportsNotificationsInterface {
+	
+	
+	
+	
+	
+
+}
